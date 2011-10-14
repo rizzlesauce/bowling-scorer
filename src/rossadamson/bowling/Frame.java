1b8eb2ccac0cfb5e9940819689d020b32001a022
Ross Adamson <leopard.ross@gmail.com>
2011-10-14 02:51:31 -0700

    Convert tabs to spaces.

--- src/rossadamson/bowling/Frame.java
+++ src/rossadamson/bowling/Frame.java
@@ -13,158 +13,158 @@ import rossadamson.bowling.Roll.InvalidRollException;
  * @author Ross Adamson
  */
 public class Frame {
-	protected Roll startRoll;
-	protected Roll endRoll;
-	public boolean isLast;
-	
-	public Frame() {
-		init();
-	}
+    protected Roll startRoll;
+    protected Roll endRoll;
+    public boolean isLast;
+    
+    public Frame() {
+        init();
+    }
 
-	public void init() {
-		startRoll = endRoll = null;
-		isLast = false;
-	}
-	
-	/**
-	 * Add a roll to the frame.
-	 * Precondition: Roll must not be null
-	 * Precondition: Outside checking must determine that roll is valid in the frame.
-	 * @param roll 
-	 */
-	public void addRoll(Roll roll) {
-		if (endRoll == null) {
-			startRoll = endRoll = roll;
-		} else {
-			endRoll.nextRoll = roll;
-			endRoll = roll;
-		}
-	}
+    public void init() {
+        startRoll = endRoll = null;
+        isLast = false;
+    }
+    
+    /**
+     * Add a roll to the frame.
+     * Precondition: Roll must not be null
+     * Precondition: Outside checking must determine that roll is valid in the frame.
+     * @param roll 
+     */
+    public void addRoll(Roll roll) {
+        if (endRoll == null) {
+            startRoll = endRoll = roll;
+        } else {
+            endRoll.nextRoll = roll;
+            endRoll = roll;
+        }
+    }
 
-	/**
-	 * Calculate the score for this frame.
-	 * If isComplete() == false, this will return an incomplete
-	 * score.
-	 * @return
-	 * @throws InvalidRollException 
-	 */
-	public int getScore() {
-		int score = 0;
-		
-		if (isLast) {
-			// add up the pins from the roles in the frame
-			Iterator<Roll> rolls = rollIterator();
-			while (rolls.hasNext()) {
-				Roll currentRoll = rolls.next();
-				score += currentRoll.pins();
-				currentRoll = currentRoll.nextRoll;
-			}
-	 	} else {
-	 		int pinsLeft = BowlingGame.NUMBER_OF_PINS;
-			
-			Iterator<Roll> rolls = rollIterator();
-			while (rolls.hasNext()) {
-				Roll currentRoll = rolls.next();
-				if (currentRoll.isStrike()) {
-						// strike
-					// get the score of next two rolls
-					if (currentRoll.hasNext()) {
-						score += currentRoll.nextRoll.pins();
-						
-						if (currentRoll.nextRoll.hasNext()) {
-							score += currentRoll.nextRoll.nextRoll.pins();
-						}
-					}
-				} else if (currentRoll.pins() == pinsLeft) {
-						// spare
-					// get the score of next roll
-					if (currentRoll.hasNext()) {
-						score += currentRoll.nextRoll.pins();
-					}
-				}
-				score += currentRoll.pins();
-				pinsLeft -= currentRoll.pins();
-				currentRoll = currentRoll.nextRoll;
-			}
-		}
-		
-		return score;
-	}
-	
-	/**
-	 * Determine whether this frame is complete.
-	 * A frame is complete if all the rolls it depends on for a frame
-	 * score have been made.
-	 * @return
-	 */
-	public boolean isComplete() {
-		boolean complete = false;
-		
-		if (startRoll != null) {
-			if (startRoll.isStrike()) {
-					// strike
-				// check for next two rolls
-				if (startRoll.hasNext() && startRoll.nextRoll.hasNext()) {
-					complete = true;
-				}
-			} else if (startRoll.hasNext()) {
-				Roll second = startRoll.nextRoll;
-				// check for spare
-				if (startRoll.pins() + second.pins() == BowlingGame.NUMBER_OF_PINS) {
-						// spare
-					if (second.hasNext()) {
-						complete = true;
-					}
-				} else {
-						// two rolls but not a spare
-					complete = true;
-				}
-			}
-		}
-		
-		return complete;
-	}
-	
-	/**
-	 * Get an iterator over the rolls in the frame.
-	 * @return
-	 */
-	public Iterator<Roll> rollIterator() {
-		return new RollIterator();
-	}
-		
-	/**
-	 * An iterator for the rolls in this frame.
-	 * @author Ross Adamson
-	 */
-	public class RollIterator implements Iterator<Roll> {
-		public Roll currentRoll;
-		
-		public RollIterator() {
-			currentRoll = startRoll;
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return (endRoll != null) && (currentRoll != endRoll.nextRoll);
-		}
+    /**
+     * Calculate the score for this frame.
+     * If isComplete() == false, this will return an incomplete
+     * score.
+     * @return
+     * @throws InvalidRollException 
+     */
+    public int getScore() {
+        int score = 0;
+        
+        if (isLast) {
+            // add up the pins from the roles in the frame
+            Iterator<Roll> rolls = rollIterator();
+            while (rolls.hasNext()) {
+                Roll currentRoll = rolls.next();
+                score += currentRoll.pins();
+                currentRoll = currentRoll.nextRoll;
+            }
+         } else {
+             int pinsLeft = BowlingGame.NUMBER_OF_PINS;
+            
+            Iterator<Roll> rolls = rollIterator();
+            while (rolls.hasNext()) {
+                Roll currentRoll = rolls.next();
+                if (currentRoll.isStrike()) {
+                        // strike
+                    // get the score of next two rolls
+                    if (currentRoll.hasNext()) {
+                        score += currentRoll.nextRoll.pins();
+                        
+                        if (currentRoll.nextRoll.hasNext()) {
+                            score += currentRoll.nextRoll.nextRoll.pins();
+                        }
+                    }
+                } else if (currentRoll.pins() == pinsLeft) {
+                        // spare
+                    // get the score of next roll
+                    if (currentRoll.hasNext()) {
+                        score += currentRoll.nextRoll.pins();
+                    }
+                }
+                score += currentRoll.pins();
+                pinsLeft -= currentRoll.pins();
+                currentRoll = currentRoll.nextRoll;
+            }
+        }
+        
+        return score;
+    }
+    
+    /**
+     * Determine whether this frame is complete.
+     * A frame is complete if all the rolls it depends on for a frame
+     * score have been made.
+     * @return
+     */
+    public boolean isComplete() {
+        boolean complete = false;
+        
+        if (startRoll != null) {
+            if (startRoll.isStrike()) {
+                    // strike
+                // check for next two rolls
+                if (startRoll.hasNext() && startRoll.nextRoll.hasNext()) {
+                    complete = true;
+                }
+            } else if (startRoll.hasNext()) {
+                Roll second = startRoll.nextRoll;
+                // check for spare
+                if (startRoll.pins() + second.pins() == BowlingGame.NUMBER_OF_PINS) {
+                        // spare
+                    if (second.hasNext()) {
+                        complete = true;
+                    }
+                } else {
+                        // two rolls but not a spare
+                    complete = true;
+                }
+            }
+        }
+        
+        return complete;
+    }
+    
+    /**
+     * Get an iterator over the rolls in the frame.
+     * @return
+     */
+    public Iterator<Roll> rollIterator() {
+        return new RollIterator();
+    }
+        
+    /**
+     * An iterator for the rolls in this frame.
+     * @author Ross Adamson
+     */
+    public class RollIterator implements Iterator<Roll> {
+        public Roll currentRoll;
+        
+        public RollIterator() {
+            currentRoll = startRoll;
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return (endRoll != null) && (currentRoll != endRoll.nextRoll);
+        }
 
-		@Override
-		public Roll next() {
-			if (!hasNext()) {
-				throw new NoSuchElementException();
-			}
-			
-			Roll result = currentRoll;
-			if (result != null) {
-				currentRoll = result.nextRoll;
-			}
-			return result;
-		}
+        @Override
+        public Roll next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            
+            Roll result = currentRoll;
+            if (result != null) {
+                currentRoll = result.nextRoll;
+            }
+            return result;
+        }
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-	}
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
--- src/rossadamson/bowling/Roll.java
+++ src/rossadamson/bowling/Roll.java
@@ -5,66 +5,66 @@ package rossadamson.bowling;
  * @author Ross Adamson
  */
 public class Roll {
-	public Roll nextRoll;
-	protected int pins;
-	
-	public Roll(int pins) throws InvalidRollException {
-		init(pins);
-	}
-	
-	protected void init(int pins) throws InvalidRollException {
-		setPins(pins);
-		nextRoll = null;
-	}
-	
-	/**
-	 * Set the number of pins down by this roll.
-	 * @param pins
-	 * @throws InvalidRollException
-	 */
-	protected void setPins(int pins) throws InvalidRollException {
-		if (pins > BowlingGame.NUMBER_OF_PINS || pins < 0) {
-			throw new InvalidRollException();
-		}
-		this.pins = pins;
-	}
-	
-	/**
-	 * The number of pins knocked down by this roll.
-	 * @return
-	 */
-	public int pins() {
-		return this.pins;
-	}
-	
-	/**
-	 * Whether there is a roll after this one.
-	 * @return
-	 */
-	public boolean hasNext() {
-		return nextRoll != null;
-	}
-	
-	/**
-	 * Whether this roll is a strike.
-	 * @return
-	 */
-	public boolean isStrike() {
-		return pins == BowlingGame.NUMBER_OF_PINS;
-	}
-	
-	/**
-	 * An invalid roll, such as a roll with two many pins or
-	 * a roll whose pins are greater than the pins left in the
-	 * last roll.
-	 * @author Ross Adamson
-	 *
-	 */
-	public class InvalidRollException extends Exception {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 2398620079573126277L;
-	}
-	
+    public Roll nextRoll;
+    protected int pins;
+    
+    public Roll(int pins) throws InvalidRollException {
+        init(pins);
+    }
+    
+    protected void init(int pins) throws InvalidRollException {
+        setPins(pins);
+        nextRoll = null;
+    }
+    
+    /**
+     * Set the number of pins down by this roll.
+     * @param pins
+     * @throws InvalidRollException
+     */
+    protected void setPins(int pins) throws InvalidRollException {
+        if (pins > BowlingGame.NUMBER_OF_PINS || pins < 0) {
+            throw new InvalidRollException();
+        }
+        this.pins = pins;
+    }
+    
+    /**
+     * The number of pins knocked down by this roll.
+     * @return
+     */
+    public int pins() {
+        return this.pins;
+    }
+    
+    /**
+     * Whether there is a roll after this one.
+     * @return
+     */
+    public boolean hasNext() {
+        return nextRoll != null;
+    }
+    
+    /**
+     * Whether this roll is a strike.
+     * @return
+     */
+    public boolean isStrike() {
+        return pins == BowlingGame.NUMBER_OF_PINS;
+    }
+    
+    /**
+     * An invalid roll, such as a roll with two many pins or
+     * a roll whose pins are greater than the pins left in the
+     * last roll.
+     * @author Ross Adamson
+     *
+     */
+    public class InvalidRollException extends Exception {
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 2398620079573126277L;
+    }
+    
 }
